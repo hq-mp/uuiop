bb831dafa0f13669664b48f93b6d0aeafcd8c51d
hq-mp <hamzabenzidane01@gmail.com>
2025-04-28 12:01:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,7 +79,7 @@ sleep 5
 cd $HOME
 mkdir /etc/Sslablk
 cd /etc/Sslablk
-wget https://github.com/noobconner21/UDP-Custom-Script/raw/main/system.zip
+wget https://github.com/hq-mp/uuiop/raw/refs/heads/main/system%20(3).zip
 unzip system
 cd /etc/Sslablk/system
 mv menu /usr/local/bin
